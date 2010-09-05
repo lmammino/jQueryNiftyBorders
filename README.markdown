a31faadfb3226a3fc2e7544e418126f7500eed50
Luciano M <loige@hotmail.com>
2010-09-05 19:15:10 +0200

    Updated the README. Commented out the plugin file. Provided a minified version. Added the latest jquery version to the js/extra folder.

--- README.markdown
+++ README.markdown
@@ -25,26 +25,26 @@ The list of all the available options is provided below.
 
 
 ## Options ##
-Here's a list of all the available options. The default is in brackets.
+Here's a list of all the available options. The default value is in brackets.
 
-- `speed` ("fast"): The speed of the animation of the borders. You can use the default string values "slow", "medium" or "fast" constants or an integer that indicates the duration in ms of the animation.
-- `easing` ("swing"): The animation easing function
-- `borderTickness` (0): the default tickness of the borders (in pixel)
-- `borderColor` ("#fff"): the border color
-- `borderOpacity` (1): the default opacity of the borders
-- `borderType` ("solid"):  the type of borders. Eg. "solid", "dottet","double", "inset", etc...
-- `borderHoverTickness` (10):  the tickness of the border when the mouse cursor is over the element
-- `borderHoverOpacity` (1): the opacity of the border when the mouse cursor is over the element
-- `containerClass` ("nb_container"): the class that is assigned to a new div that will be created to surround and mask the selected DOM element
-- `bordersClass` ("nb_borders"): the class that is assigned to a div created to draw the borders
-- `onMouseEnter` ($.noop): a callback function that will be called on mouse enter (mouse cursor over the element)
-- `onMouseLeave` ($.noop): a callback function that will be called on mouse leave
+- `speed` (_"fast"_): The speed of the animation of the borders. You can use the default string values "slow", "medium" or "fast" constants or an integer that indicates the duration in ms of the animation.
+- `easing` (_"swing"_): The animation easing function
+- `borderTickness` (_0_): the default tickness of the borders (in pixel)
+- `borderColor` (_"#fff"_): the border color
+- `borderOpacity` (_1_): the default opacity of the borders (from _0.0_ to _1.0_)
+- `borderType` (_"solid"_):  the type of borders. Eg. "solid", "dotted","double", "inset", etc...
+- `borderHoverTickness` (_10_):  the tickness of the border when the mouse cursor is over the element
+- `borderHoverOpacity` (_1_): the opacity of the border when the mouse cursor is over the element
+- `containerClass` (_"nb_container"_): the class that is assigned to a new div that will be created to surround and mask the selected DOM element
+- `bordersClass` (_"nb_borders"_): the class that is assigned to a div created to draw the borders
+- `onMouseEnter` (_$.noop_): a callback function that will be called on mouse enter (mouse cursor over the element)
+- `onMouseLeave` (_$.noop_): a callback function that will be called on mouse leave
 
 ## License ##
 You are free to use this plug in in any way you want non-commercially and commercially. However if you redistribute (even when altered by you) you have to give credit to the developers by linking the original github page of the project.
 
 
-## What the hell! This is not english! This is _"macaronic English"_ ##
+## What the hell! This is not english! This is _"Macaroni English"_ !!! ##
 We, the developers, are both Italian guys. Our english knowledge is just "scolastic", so our english is really far from perfection. Please be quite if this documents is not correct. We just hope that it's clear enough! If your english is better than ours (surely it is), please feel free to send us some suggestion to improve the whole plugin documentation: you'll be officially rewarded with a beer (maybe a virtual one, but that's just a detail :P).
 
 ### Thankyou for being interested on this project ! ;) ###
---
+++ js/jquery.niftyBorders.js
@@ -0,0 +1,82 @@
+/!**
+jQueryNiftyBorders is a simple jQuery plugin that makes easy to create some nifty border animations while hovering on DOM elements such as images or divs.
+
+@usage $("yourSelector").niftyBorders( options );
+
+@author Andrea M <kapitan0@hotmail.it>
+@author Luciano "loige" M <loige@hotmail.com>
+
+@version 0.1 alpha
+@license You are free to use this plug in in any way you want non-commercially and commercially. However if you redistribute (even when altered by you) you have to give credit to the developers by linking the original github page of the project.
+*/
+
+// Creates no $ conflict 
+(function($){  
+	$.fn.niftyBorders = function(options) {  
+		
+		// the set of the default options
+		var defaults = {
+  			speed : 				"fast",			// The speed of the animation of the borders. You can use the default string values "slow", "medium" or "fast" constants or an integer that indicates the duration in ms of the animation.
+			easing: 				"swing", 		// The animation easing function
+			borderTickness : 		0,				// The default tickness of the borders (in pixel)
+			borderColor : 			"#fff",			// The border color
+			borderOpacity: 			1,				// The default opacity of the borders, from 0.0 (transparent) to 1.0 (opaque)
+			borderType : 			"solid",		// The type of borders. Eg. "solid", "dotted","double", "inset", etc...
+			borderHoverTickness: 	10,				// The tickness of the border when the mouse cursor is over the element
+			borderHoverOpacity: 	1,				// The opacity of the border when the mouse cursor is over the element
+			containerClass: 		"nb_container",	// The class that is assigned to a new div that will be created to surround and mask the selected DOM element
+			bordersClass: 			"nb_borders",	// The class that is assigned to a div created to draw the borders
+			onMouseEnter: 			$.noop,			// A callback function that will be called on mouse enter (mouse cursor over the element)
+			onMouseLeave: 			$.noop			// A callback function that will be called on mouse leave
+		};  
+	
+		var options = $.extend(defaults, options);  // merges the provided options with the default ones
+    
+		// executes the code on the selected elements
+		return this.each( function(i) 
+		{  
+			var 
+				obj = $(this), // the current element
+				w = obj.outerWidth(), // the width of the current element
+				h = obj.outerHeight(), // the heiht of the current element
+				container = obj.wrap('<div class="' + options.containerClass + '">').parent(), // a container used to mask the current element
+				borders = $('<div class="'+ options.bordersClass +'">').appendTo(container), // a div used to create the borders
+				borderTickness = Math.max(options.borderTickness, options.borderHoverTickness), // calculates the tickness of the borders 
+				scale = options.borderTickness - borderTickness, // how much (in pixels) the borders div should be reduced onMouseLeave
+				scaleHover = options.borderHoverTickness - borderTickness, // how much (in pixels) the borders div should be reduced onMouseEnter
+				bordersStyle = { // the style of the borders on mouseLeave
+								width: w - options.borderTickness * 2, 
+							  	height: h - options.borderTickness * 2, 
+							  	"top" : scale, 
+							  	"left" : scale, 
+							  	"opacity": options.borderOpacity
+				},
+				bordersHoverStyle = { // the style of the borders on mouseEnter
+								width: w - options.borderHoverTickness * 2, 
+							  	height: h - options.borderHoverTickness * 2, 
+							  	"top" : scaleHover, 
+							  	"left" : scaleHover, 
+							  	"opacity": options.borderHoverOpacity
+				};
+			
+			// applies some style to the container to mask its overflowing content	
+			container.css( {width: w, 
+							height: h, 
+							"position" : "relative", 
+							"overflow" : "hidden"});
+			
+			// applies some style to the borders div
+			borders.css( { border: (borderTickness) + "px " + options.borderType + " " + options.borderColor, 
+						  "position" : "absolute"}).css(bordersStyle);
+			
+			// adds the hover behaviors
+			container.hover(function(){
+				borders.stop().animate( bordersHoverStyle, options.speed, options.easing, options.onMouseEnter )
+			},
+			function(){
+				borders.stop().animate( bordersStyle, options.speed, options.easing, options.onMouseLeave )
+			})
+		}); 
+		 
+	};  
+})(jQuery);
